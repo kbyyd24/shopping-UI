55aa99b419a0d1d1e1497e00491ebd1cad05a29e
polang <grzx2210@163.com>
2016-07-02 18:07:13 +0800

    [N/A] move project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shoppingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shoppingApp
+ */
+angular.module('shoppingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shoppingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shoppingApp
+ */
+angular.module('shoppingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
